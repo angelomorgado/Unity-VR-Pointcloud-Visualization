5ae0a7267e6cb64bdfc1a4f4d4dfd734fe14c983
AmrMorgado <angelo.morgado@ubi.pt>
2023-04-23 12:13:00 +0100

    Added rotation through input

---
+++ Assets/RodarCP.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RodarCP : MonoBehaviour
+{
+    public InputActionReference rotateActionReference;
+    public float rotateForce = 500.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateActionReference.action.performed += OnRotate;
+    }
+
+    private void OnRotate(InputAction.CallbackContext obj)
+    {
+        transform.Rotate(new Vector3(0.0f,1.0f,0.0f) * Time.deltaTime * rotateForce);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0.0f,1.0f,0.0f) * Time.deltaTime * rotateForce);
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0.0f,1.0f,0.0f) * Time.deltaTime * -rotateForce);
+        }
+
+        // Reset
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
